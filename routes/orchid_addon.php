e12e4e893dce6e52cdae745205a2a89ff6bcb906
Christian Quispe H <christian.quispeh@gmail.com>
2026-05-06 20:54:44 -0500

    Enhance breadcrumbs for log screen with file path

--- routes/orchid_addon.php
+++ routes/orchid_addon.php
@@ -16,10 +16,18 @@ Route::screen('phpinfo', \OrchidAddon\Screens\PhpinfoScreen::class)
 // Platform > Logs
 Route::screen('logs', \OrchidAddon\Screens\Log\LogListScreen::class)
     ->name('platform.logs')
-    ->breadcrumbs(function (\Tabuna\Breadcrumbs\Trail $trail) {
-        return $trail
-            ->parent('platform.index')
-            ->push(__('Logs'), route('platform.logs'));
+    ->breadcrumbs(function (\Tabuna\Breadcrumbs\Trail $trail, $file_name) {
+
+        $trail = $trail->parent('platform.logs');
+        $parts = explode('/', $file_name);
+        $currentPath = '';
+    
+        foreach ($parts as $part) {
+            $currentPath .= ($currentPath ? '/' : '') . $part;
+            $trail->push($part);
+        }
+
+        return $trail;
     });
 
 Route::screen('logs/{file_name}/preview', \OrchidAddon\Screens\Log\LogPreviewScreen::class)
