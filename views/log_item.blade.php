98809fceefbbf4950f6ff311b6de7d15d7e986dc
Christian Quispe H <christian.quispeh@gmail.com>
2026-05-06 21:12:53 -0500

    Add alert before highlight.js initialization

--- views/log_item.blade.php
+++ views/log_item.blade.php
@@ -26,5 +26,8 @@
     <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-    <script>hljs.highlightAll();</script>
+    <script>
+        alert('funciona?')
+        hljs.highlightAll();
+    </script>
 @endsection
